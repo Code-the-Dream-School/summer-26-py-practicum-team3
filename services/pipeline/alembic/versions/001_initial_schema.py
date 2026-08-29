e7c6f79f0feac4b99991e532001ba1da39f205e3
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2026-08-29 00:34:29 -0700

    AIR-19 Enforce NOT NULL on air_pollution_gold lat/lon

--- services/pipeline/alembic/versions/001_initial_schema.py
+++ services/pipeline/alembic/versions/001_initial_schema.py
@@ -197,8 +197,8 @@ def upgrade():
             o3              DOUBLE PRECISION,
             so2              DOUBLE PRECISION,
             nh3             DOUBLE PRECISION,
-            lat             DOUBLE PRECISION,
-            lon             DOUBLE PRECISION,
+            lat             DOUBLE PRECISION NOT NULL,
+            lon             DOUBLE PRECISION NOT NULL,
             retrieved_at    TIMESTAMPTZ NOT NULL,
 
             CONSTRAINT air_pollution_gold_pk
